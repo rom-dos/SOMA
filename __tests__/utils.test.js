3ccf59fc5fcbf2d56ca6c7e2a2fc5e76fbea60c9
Nicholas Matthew Morera <morera.computer@gmail.com>
2019-01-10 21:01:16 -0800

    add initial tests in Jest for quantSet

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,11 +1,26 @@
 /* eslint-env jest */
 const harmonicSets = require('../src/harmonicSets')
-const utils = require('../src/utils')
+const { quantSet } = require('../src/utils')
 
 /* -- quantSet() -- */
 describe('quantSet', () => {
-  test('quantizes the set in 1/4 notes', () => {
-    const test = utils.quantSet(harmonicSets.major['c'][1])
+  test('returns an object', () => {
+    const test = quantSet(harmonicSets.minor['db'][1], 16)
+    expect(typeof test).toEqual('object')
+  })
+
+  test('quantizes the set in 1/4 notes when given an argument for quant', () => {
+    const test = quantSet(harmonicSets.major['c'][1], 4)
     expect(test).toEqual(['c4', 'd', 'e', 'f', 'g', 'a', 'b'])
+  })
+
+  test('quantizes the set in 1/4 notes when using the default parameter for quant', () => {
+    const test = quantSet(harmonicSets.major['db'][1])
+    expect(test).toEqual(['des4', 'ees', 'f', 'ges', 'aes', 'bes', 'c'])
+  })
+
+  test('quantizes the set in 1/32 notes', () => {
+    const test4 = quantSet(harmonicSets.minor['eb'][1], 32)
+    expect(test4).toEqual(['ees32', 'f', 'ges', 'aes', 'bes', 'c', 'des'])
   })
 })
