dc05f59f0c356578707d4501161cf23317a77fea
Nicholas Morera <morera.computer@gmail.com>
2020-01-13 15:42:44 -0800

    Refactor quantSet tests and function itself

--- __tests__/quantSet.test.js
+++ __tests__/quantSet.test.js
@@ -1,26 +1,32 @@
 /* eslint-env jest */
-const harmonicSets = require('../src/harmonicSets')
 const { quantSet } = require('../src/utils')
 
 /* -- quantSet -- */
 describe('quantSet', () => {
-  test.skip('returns an object', () => {
-    const test = quantSet(harmonicSets.minor['db'][1], 16)
+  test('returns an object', () => {
+    const set = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+    const test = quantSet(set, 16)
     expect(typeof test).toEqual('object')
   })
 
-  test.skip('quantizes the set in 1/4 notes when given an argument for quant', () => {
-    const test = quantSet(harmonicSets.major['c'][1], 4)
-    expect(test).toEqual(['c4', 'd', 'e', 'f', 'g', 'a', 'b'])
+  test('quantizes the set in 1/4 notes when given an argument for quant', () => {
+    const set = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+    const test = quantSet(set, 4)
+    const expected = ['c4', 'd', 'e', 'f', 'g', 'a', 'b']
+    expect(test).toEqual(expected)
   })
 
-  test.skip('quantizes the set in 1/4 notes when using the default parameter for quant', () => {
-    const test = quantSet(harmonicSets.major['db'][1])
-    expect(test).toEqual(['des4', 'ees', 'f', 'ges', 'aes', 'bes', 'c'])
+  test('quantizes the set in 1/4 notes when using the default parameter for quant', () => {
+    const set = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+    const test = quantSet(set)
+    const expected = ['c4', 'd', 'e', 'f', 'g', 'a', 'b']
+    expect(test).toEqual(expected)
   })
 
-  test.skip('quantizes the set in 1/32 notes', () => {
-    const test = quantSet(harmonicSets.minor['eb'][1], 32)
-    expect(test).toEqual(['ees32', 'f', 'ges', 'aes', 'bes', 'c', 'des'])
+  test('quantizes the set in 1/32 notes', () => {
+    const set = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+    const test = quantSet(set, 32)
+    const expected = ['c32', 'd', 'e', 'f', 'g', 'a', 'b']
+    expect(test).toEqual(expected)
   })
 })
--- src/utils.js
+++ src/utils.js
@@ -4,13 +4,7 @@ const harmonicSets = require('./harmonicSets')
 // 1 = whole note, 2 = 1/2 note, 4 = 1/4 note, 8 = 1/8 note
 // 16 = 1/16 note, 32 = 1/32 note, 64 = 1/64 note, 128 = 1/128 note
 // default quantization set to 1/4 notes
-const quantSet = (set, quant = 4) => {
-  const localSet = set
-  let first = localSet[0]
-  first += quant
-  localSet[0] = first
-  return localSet
-}
+const quantSet = (set, quant = 4) => [set.slice(0, 1)[0].concat(quant), ...set.slice(1)]
 
 /* -- playScale -- */
 const playScale = (set, quant = 4) => {
