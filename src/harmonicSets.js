b239f5611d4bc160988792b2ae43ce690f6b75f3
Nicholas Morera <morera.computer@gmail.com>
2020-05-20 01:19:18 -0700

    Add Harmonic Minor scale to harmonicSets

--- src/harmonicSets.js
+++ src/harmonicSets.js
@@ -5,5 +5,6 @@ module.exports = {
   lydian: [0, 2, 4, 6, 7, 9, 11],
   mixolydian: [0, 2, 4, 5, 7, 9, 10],
   aeolian: [0, 2, 3, 5, 7, 8, 10],
-  locrian: [0, 1, 3, 5, 6, 8, 10]
+  locrian: [0, 1, 3, 5, 6, 8, 10],
+  harmonicMinor: [0, 2, 3, 5, 7, 8, 11]
 }
