acdfdf8e469b05ac57b71e736eaf710b46bc0f30
Nicholas Morera <morera.computer@gmail.com>
2020-05-22 20:07:22 -0700

    I can print a list of available scale types.

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ const {
   readScore
 } = require('./utils.js')
 const { chordTypes } = require('./chordTypes.js')
+const { scaleTypes } = require('./scaleTypes.js')
 
 shell.mkdir('-p', `${homedir}/soma-output`)
 shell.mkdir('-p', `${homedir}/.cache/soma`)
@@ -253,12 +254,19 @@ program
   .alias('list')
   .description('List various information.')
   .option('--chords', 'Display list of available chords.')
+  .option('--scales', 'Display list of available scales.')
   .action(options => {
     if (options.chords) {
       console.log('')
       console.log('Available chords:')
       console.log('')
       chordTypes.map(x => console.log(x.name))
+    }
+    if (options.scales) {
+      console.log('')
+      console.log('Available scales:')
+      console.log('')
+      scaleTypes.map(x => console.log(x.name))
     }
   })
 
---
+++ src/scaleTypes.js
@@ -0,0 +1,14 @@
+const scaleTypes = [
+  { name: 'Ionian', short: 'ionian' },
+  { name: 'Dorian', short: 'dorian' },
+  { name: 'Phrygian', short: 'phrygian' },
+  { name: 'Lydian', short: 'lydian' },
+  { name: 'Mixolydian', short: 'mixolydian' },
+  { name: 'Aeolian', short: 'aeolian' },
+  { name: 'Locrian', short: 'locrian' },
+  { name: 'Harmonic Minor', short: 'harmonicMinor' }
+]
+
+module.exports = {
+  scaleTypes
+}
